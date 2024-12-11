55b12b04890d3bc11a854f07b72c149d3dd20283
abdul <choudhuryabdul28@gmail.com>
2024-12-11 00:22:40 -0500

    This is for Develop Brach

---
+++ SaturdayFury/src/developBranch/Test.java
@@ -0,0 +1,11 @@
+package developBranch;
+
+public class Test {
+	public static void practiceForLoop(){					//For Loop - Do again and again upto upper limit
+		int i;												//Initialize the variable
+		for(i=1; i<=10; i++){								//Setting lower limit, Upper limit, Increment or Decrement
+			System.out.println("For Loops number = " + i);	//Statement
+		}
+	}
+
+}
